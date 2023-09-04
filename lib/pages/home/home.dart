5cae0405196b610de1fb316e22abba5179218f56
A.Vassel <102523970+AVassel@users.noreply.github.com>
2023-09-04 19:44:52 +0300

    working app

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -47,11 +47,16 @@ class Home extends StatelessWidget {
           ), 
         ],
       ),
-      body: BrewList(),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/coffee_bg.png'),
+            fit: BoxFit.cover,
+          )
+        ),
+        child: BrewList()
+      ),
     ),
-      );
-    //return StreamProvider<QuerySnapshot>.value(
-      //initialData: null, value: null,
-    //);
+   );
   }
 }
