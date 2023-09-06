a51a91ea7cc92adb0fce3f1121154f49561b3b52
A.Vassel <102523970+AVassel@users.noreply.github.com>
2023-09-06 10:55:36 +0300

    working app

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -7,3 +7,4 @@ const TextInputDecoration = InputDecoration(
                 enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white, width: 2.0)),
                 focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.pink, width: 2.0)),
               );
+
